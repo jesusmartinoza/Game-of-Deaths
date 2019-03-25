ae0e6a5d531abe79ac673eadaeb98e4a78d32968
Jesús Martínez <hongo-loco@hotmail.com>
2019-03-25 09:40:33 -0600

    MainScreen fuentes

---
+++ src/components/FancyButton.vue
@@ -0,0 +1,59 @@
+<template>
+    <a href="#" class="button">Start</a>
+</template>
+
+<script>
+export default {
+    name : 'FancyButton'
+}
+</script>
+
+<style scoped lang="scss">
+$color: #FFB309;
+
+@keyframes sheen {
+  0% {
+    transform: skewY(-45deg) translateX(0);
+  }
+  100% {
+    transform: skewY(-45deg) translateX(12.5em);
+  }
+}
+
+.button {
+  padding: 0.5em 2em;
+  text-align: center;
+  text-decoration: none;
+  color: $color;
+  border: 2px solid $color;
+  font-size: 24px;
+  display: inline-block;
+  border-radius: 0.6em;
+  transition: all 0.2s ease-in-out;
+  position: relative;
+  overflow: hidden;
+  margin-top: 1em;
+  font-family: "Cinzel";
+  &:before {
+    content: "";
+    background-color: rgba(255,255,255,0.5);
+    height: 100%;
+    width: 3em;
+    display: block;
+    position: absolute;
+    top: 0;
+    left: -4.5em;
+    transform: skewX(-45deg) translateX(0);
+    transition: none;
+  }
+  &:hover {
+    background-color: $color;
+    color: #fff;
+    border-bottom: 4px solid darken($color, 10%);
+    &:before {
+      transform: skewX(-45deg) translateX(13.5em);
+     transition: all 0.5s ease-in-out;
+    }
+  }
+}
+</style>
---
+++ src/components/MainScreen.vue
@@ -0,0 +1,122 @@
+<template>
+  <div>
+    <img id="logo" src="../assets/game_of_deaths.png" alt="Game of Deaths">
+
+    <h1>
+      Play with your <br>
+      friends about the final season <br>
+      of <br>
+      Game of Thrones!
+    </h1>
+
+    <FancyButton />
+
+    <footer>
+      <p>
+        Site made by fans
+      </p>
+      <!--p>
+        There are so many visitors and web hosting are mountin’ up! So this site is supported by ads
+      </p-->
+    </footer>
+
+    <div class="crossfade">
+      <figure></figure>
+      <figure></figure>
+      <figure></figure>
+      <figure></figure>
+      <figure></figure>
+    </div>
+  </div>
+</template>
+
+<script>
+import FancyButton from '../components/FancyButton.vue'
+
+export default {
+    name : 'MainScreen',
+    components: {
+      FancyButton
+    }
+}
+</script>
+
+<style scoped>
+#logo {
+  max-width: 400px;
+  width: 100%;
+}
+
+h1 {
+  font-size: 1.3rem;
+  margin-top: 15vh;
+  text-shadow: 0px 0px 4px #ffaa;
+}
+
+footer {
+  bottom: 0;
+  font-family: "Avenir";
+  font-size: 14px;
+  left: 0;
+  margin-top: 2em;
+  padding: 1em;
+}
+
+/* Background animation */
+.crossfade > figure {
+  animation: imageAnimation 30s linear infinite 0s;
+  backface-visibility: hidden;
+  background-size: cover;
+  background-position: center center;
+  color: transparent;
+  height: 100vh;
+  left: 0px;
+  margin: 0;
+  opacity: 0;
+  position: absolute;
+  top: 0px;
+  width: 100%;
+  z-index: -9999;
+}
+
+.crossfade > figure:nth-child(1) { background-image: url('https://wallpapercave.com/wp/wp2062228.jpg'); }
+
+.crossfade > figure:nth-child(2) {
+  animation-delay: 6s;
+  background-image: url('https://wallpapercave.com/wp/wp2062229.jpg');
+}
+
+.crossfade > figure:nth-child(3) {
+  animation-delay: 12s;
+  background-image: url('https://wallpapercave.com/wp/wp2062334.jpg');
+}
+
+.crossfade > figure:nth-child(4) {
+  animation-delay: 18s;
+  background-image: url('https://wallpapercave.com/wp/wp2062291.jpg');
+}
+
+.crossfade > figure:nth-child(5) {
+  animation-delay: 24s;
+  background-image: url('https://wallpapercave.com/wp/wp2062265.jpg');
+}
+@keyframes
+imageAnimation {  0% {
+ animation-timing-function: ease-in;
+ opacity: 0;
+}
+ 8% {
+ animation-timing-function: ease-out;
+ opacity: 0.3;
+}
+ 17% {
+ opacity: 0.35;
+}
+ 25% {
+ opacity: 0
+}
+ 100% {
+ opacity: 0
+}
+}
+</style>
